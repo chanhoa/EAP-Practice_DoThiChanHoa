50c83b093182688b252ab9a8bdae80ad9cb08f8b
Practice-KhongTen <59854810+Practice-KhongTen@users.noreply.github.com>
2021-01-21 09:14:57 +0700

    chanhoa

---
+++ ManageEmployees/ManageEmployees/Migrations/20210121021054_DBv1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManageEmployees.Migrations
+{
+    public partial class DBv1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "ID", "Department", "Image", "Name", "Phone" },
+                values: new object[,]
+                {
+                    { 1, "Phòng Kế toán", "Do-thi-chan-hoa.jpg", "Đổ Thị Chan Hòa", "012369856" },
+                    { 2, "Phòng bảo vệ", "Tuan.jpg", "Phạm Tuân", "012369856" },
+                    { 3, "Phòng bảo vệ", "Vu-quang-huy.jpg", "Vũ Quang Huy", "012369856" },
+                    { 4, "Phòng công nghệ thông tin", "Hieu-icetea.jpg", "Nguyễn Đình Hiếu", "012369856" },
+                    { 5, "Phòng quản lý", "truong-thanh-tu.jpg", "Trương Thanh Tú", "012369856" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ ManageEmployees/ManageEmployees/Models/EF/EmployeesDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ManageEmployees.Models.Configuration;
+using ManageEmployees.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageEmployees.Models.EF
+{
+    public class EmployeesDbContext:DbContext
+    {
+        public EmployeesDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+            modelBuilder.ApplyConfiguration(new EmployeesConfiguration());
+            modelBuilder.Seed();
+        }
+        public DbSet<Employees> Employees{ get; set; }
+
+    }
+}
